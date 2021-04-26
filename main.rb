29bd23759b0012f88a1619aefddd1b0df12ebe3a
Tetyana Titovets <tittan7@gmail.com>
2021-04-26 16:28:08 -0400

    Complete Question class

--- main.rb
+++ main.rb
@@ -2,14 +2,15 @@ require "./players.rb";
 require "./question.rb";
 require "./game.rb";
 
-# in the main file we will create instances of class Player (player1, player2)
-# a varibale to hold score of each player 
-
 def set_game
   player1 = Players.new
   player2 = Players.new
-  puts player1.score
+  # puts player1.score
 
+  q = Question.new
+  q.check_answer()
+  
 end
 
 set_game
+
--- question.rb
+++ question.rb
@@ -2,18 +2,34 @@
 # 2. class Question
 # attr_accessor (or attr_reader ?) :evaluate
 
-#   methods: 
-#   1. def inititalize
-#     @first_number = rand(1..20)
-#     @second_number = rand(1..20)
-#     @solution = @first_number + @second_number
-#     @question = "What does #{@first_number} plus #{@second_number}} equal?"
-#     @answer // what the user types in
-#     @evaluate // initially nil 
-    
-#   2. def check_answer
+class Question
 
-#   In here we will put a question
-#   take a response from the command line
-#   compare answer to solution
-#   give response to the player
+  attr_reader :evaluate
+  
+  def initialize()
+  
+    @first_number = rand(1..20)
+    @second_number = rand(1..20)
+    @solution = @first_number + @second_number
+    @question = "What does #{@first_number} plus #{@second_number} equal?"
+    @answer = nil
+    @evaluate = nil
+    # puts "class Question initialized"
+  end
+  
+  def check_answer 
+  
+    puts @question
+    @answer = gets.chomp.to_i
+    
+    if @answer != @solution
+      puts "You lost one life"
+      @evaluate = false
+    else 
+      puts "You gave the correct answer"
+      @evaluate = true
+    end
+  end
+end
+
+
